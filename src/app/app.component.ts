34c933e716526452ba617321c952f012b4fcfb9f
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-08-18 10:19:28 +0200

    feat: Add movie-card component and its respective templates.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+
 
 import { MatDialog } from '@angular/material/dialog';
 
@@ -12,6 +14,12 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class AppComponent {
   title = 'moobee';
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    });
+  }
 
   constructor(public dialog: MatDialog) {}
 
